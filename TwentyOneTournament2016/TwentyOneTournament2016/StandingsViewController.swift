52731e84e5e6366c99c3eda2b35e4e2cf1a8df5f
Pat McGuire <patmcguire0890@gmail.com>
2015-10-22 20:14:52 -0500

    Initial storyboard layout, added a .gitignore file, beginning Regular Season screen elements

---
+++ TwentyOneTournament2016/TwentyOneTournament2016/StandingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StandingsViewController.swift
+//  TwentyOneTournament2016
+//
+//  Created by Pat McGuire on 10/22/15.
+//  Copyright © 2015 21Tournament. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StandingsViewController: UIViewController{
+    
+    
+   
+    
+    
+}
