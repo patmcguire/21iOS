48718035b91458d2509d742d107bc858333738d2
Pat McGuire <patmcguire0890@gmail.com>
2015-11-07 14:43:02 -0600

    UI for first two rounds of playoffs.

--- TwentyOneTournament2016/TwentyOneTournament2016/PlayoffsR1.swift
+++ TwentyOneTournament2016/TwentyOneTournament2016/PlayoffsR1.swift
@@ -9,9 +9,50 @@
 import Foundation
 
 
-class PlayoffsR1: UIViewController{
+class PlayoffsR1: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    @IBOutlet var tableView: UITableView!
+    
+    let cellId = "r1Cell"
     
     
+    override func viewDidLoad() {
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 16
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellId, forIndexPath: indexPath)
+        
+        let row = indexPath.row
+        
+        if row % 2 == 0{
+            cell.backgroundView = UIImageView (image: UIImage (named: "upperBracket"))
+            cell.backgroundColor = UIColor.redColor()
+        } else{
+            cell.backgroundView = UIImageView (image: UIImage (named: "lowerBracket"))
+            cell.backgroundColor = UIColor.redColor()
+        }
+        
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let row = indexPath.row
+        print("Row: \(row+1)")
+    }
     
 }
--- TwentyOneTournament2016/TwentyOneTournament2016/PlayoffsR2.swift
+++ TwentyOneTournament2016/TwentyOneTournament2016/PlayoffsR2.swift
@@ -9,8 +9,64 @@
 import Foundation
 
 
-class PlayoffsR2: UIViewController {
+class PlayoffsR2: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+
+    @IBOutlet var tableView: UITableView!
+    
+    let cellId = "r2Cell"
+    
+    var midCells = [3,7,11,15]
     
     
+    override func viewDidLoad() {
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 16
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellId, forIndexPath: indexPath)
+        
+        let row = indexPath.row
+        
+        if (row + 1) % 4 == 0{
+            cell.backgroundView = UIImageView (image: UIImage (named: "r2lowerBracket"))
+            cell.backgroundColor = UIColor.redColor()
+        } else if (row + 1) % 2 == 0{
+            cell.backgroundView = UIImageView (image: UIImage (named: "r2topBracket"))
+            cell.backgroundColor = UIColor.redColor()
+        }else if (row + 1) % 2 != 0 {
+            for n in 0...3{
+                if row+1 == midCells[n]{
+                    cell.backgroundView = UIImageView (image: UIImage (named: "r2midBracket"))
+                    cell.backgroundColor = UIColor.redColor()
+                }else{
+                    cell.backgroundColor = UIColor.redColor()
+                }
+            }
+        }else{
+            cell.backgroundColor = UIColor.redColor()
+        }
+        
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let row = indexPath.row
+        print("Row: \(row+1)")
+    }
     
 }
